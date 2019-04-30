addd60107b30f768a7e039ba638392e0c2b05784
cytcool <675149948@qq.com>
2019-04-30 12:57:04 +0800

    数据库配置与简单实现Restful接口

--- src/main/java/net/cyt/web/italker/push/Application.java
+++ src/main/java/net/cyt/web/italker/push/Application.java
@@ -1,4 +1,22 @@
 package net.cyt.web.italker.push;
 
-public class Application {
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import net.cyt.web.italker.push.service.AccountService;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.util.logging.Logger;
+
+public class Application extends ResourceConfig {
+
+    public Application(){
+        //注册逻辑处理的包名
+        packages(AccountService.class.getPackage().getName());
+
+        //注册Json解析器
+        register(JacksonJsonProvider.class);
+
+        //注册日志打印输出
+        register(Logger.class);
+
+    }
 }
